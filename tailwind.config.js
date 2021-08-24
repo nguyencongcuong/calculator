b01b581e0d83b6103c6603b4726efec26f38c74a
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-08-24 14:35:35 +0700

    Added: customized tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,28 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
     './public/index.html'
   ],
-    darkMode: false, // or 'media' or 'class'
-      theme: {
-  extend: { },
-},
-variants: {
-  extend: { },
-},
-plugins: [
-  require('@tailwindcss/aspect-ratio'),
-],
-  }
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        cyan: {
+          100: "#eef6f6",
+          200: "#deeded",
+          300: "#cde3e4",
+          400: "#bddadb",
+          500: "#acd1d3",
+          600: "#9bc8ca",
+          700: "#8bbfc1",
+          800: "#7ab5b8",
+          900: "#6aacaf"
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
