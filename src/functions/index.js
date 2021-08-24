de01a508b4e3bbe7d202352442bf03c302f28e14
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-08-25 02:02:54 +0700

    Fixed: unable to calculate the operator with "-" at the beginning

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,6 @@
 // ? Check if the state operater matchs the string "+, -, *, /" at the beginning and the bottom of the tring
 export const isCalAble = (operatorStr) => {
-    const regex1 = new RegExp(/^(\+|-|\*|\/)|(\+|-|\*|\/)$/, "g")
+    const regex1 = new RegExp(/^(\+|\*|\/)|(\+|-|\*|\/)$/, "g")
     const regex2 = new RegExp(/\./, "g")
     const match1 = operatorStr.match(regex1)
     const match2 = operatorStr.match(regex2)
@@ -15,10 +15,4 @@ export const isCalAble = (operatorStr) => {
             return true
         }
     }
-}
-
-export const isMatchRemainder = (str) => {
-    const matchRemainderRegex = new RegExp(/%$/, "g")
-    const matched = str.match(matchRemainderRegex)
-    return matched === null ? false : true
 }
