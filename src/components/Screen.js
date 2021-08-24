7900988eb6d2fffd6bb87666a17a56ae8dee2801
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-08-25 01:42:17 +0700

    updated: style

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,22 +3,23 @@ import { useSelector } from "react-redux"
 
 function Operator() {
     const operator = useSelector(state => state.operator)
-
-
     return (
-        <span
+        <div
             id="display"
             style={{ color: "#9bc8ca" }}
-            className="text-lg h-10">
-            {operator}
-        </span>
+            className="flex flex-wrap text-lg h-10">
+            <span className="block w-full overflow-scroll scrollbar--removed">{operator}</span>
+        </div>
     )
 }
 
 function Result() {
     const result = useSelector(state => state.result)
     return (
-        <span className="text-4xl font-bold text-gray-800 h-16">{result}</span>
+        <span 
+            className="text-4xl font-bold text-gray-800 h-16">
+            <span className="block w-full overflow-scroll scrollbar--removed">{result}</span>
+        </span>
     )
 }
 
