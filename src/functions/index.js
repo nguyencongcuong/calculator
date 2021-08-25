81474897a84979c03cc2ee1c7a126d7148c96da2
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-08-25 16:44:26 +0700

    updated: function desc

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,20 +1,42 @@
-// ? Check if the state operater matchs the string "+, -, *, /" at the beginning and the bottom of the tring
-export const isCalAble = (operator) => {
-    const regex1 = new RegExp(/^(\+|\*|\/)|(\+|-|\*|\/)$/, "g")
-    const regex2 = new RegExp(/^\./, "g")
-    return (isMatched(operator, regex1) || isMatched(operator, regex2)) ? false : true
-}
-
-export function deleteLastChar(string) {
-    let strArr = string.split("")
-    strArr.pop()
-    return strArr.join("")
-}
-
+/**
+ ** Check if a string is matched by the rule of a regex
+ *  @param {string, regex}
+ *  @return {boolean} 
+ */
 export function isMatched(string, regex) {
     if (string.match(regex) !== null) {
         return true
     } else {
         return false
     }
+}
+
+/**
+ ** Check if a string of math operator is able to solve
+ *  @param {string}
+ *  @return {boolean} 
+ *
+ *? isCalAble(10) returns true
+ *? isCalAble(10+5) returns true
+ *? isCalAble(10-5) returns true
+ *? isCalAble(+10) returns false
+ *? isCalAble(12+) returns false
+ *? isCalAble(15*) returns false
+ */
+export const isCalAble = (operator) => {
+    const regex1 = new RegExp(/^(\+|\*|\/)|(\+|-|\*|\/)$/, "g")
+    const regex2 = new RegExp(/^\./, "g")
+    return (isMatched(operator, regex1) || isMatched(operator, regex2)) ? false : true
+}
+
+/**
+ ** Delete any last character from a string
+ *  @param {string} - string of math operator
+ *  @return {string} - new string excluding the last character
+ *? deleteLastChar("6*") returns "6"
+ */
+export function deleteLastChar(string) {
+    let strArr = string.split("")
+    strArr.pop()
+    return strArr.join("")
 }
