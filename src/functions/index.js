7b5f7a83ca9d7052cb1585ac58caebc51f2b614a
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-08-25 16:21:29 +0700

    updated: regex

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,20 +1,8 @@
 // ? Check if the state operater matchs the string "+, -, *, /" at the beginning and the bottom of the tring
-export const isCalAble = (operatorStr) => {
+export const isCalAble = (operator) => {
     const regex1 = new RegExp(/^(\+|\*|\/)|(\+|-|\*|\/)$/, "g")
-    const regex2 = new RegExp(/\./, "g")
-    const match1 = operatorStr.match(regex1)
-    const match2 = operatorStr.match(regex2)
-    if (match1 === null) {
-        return true
-    } else {
-        if (match1.length !== 0) {
-            return false
-        } else if (match2.length >= 2) {
-
-        } else {
-            return true
-        }
-    }
+    const regex2 = new RegExp(/^\./, "g")
+    return (isMatched(operator, regex1) || isMatched(operator, regex2)) ? false : true
 }
 
 export function deleteLastChar(string) {
