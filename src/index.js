b3a4caf069cb6fe45b162c1b803204ec6b9eb03a
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-08-27 21:21:45 +0700

    chore: Maintain code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-import allReducers from "./reducers"
 import { Provider } from "react-redux"
 import { createStore } from "redux"
+
+import './index.css';
+
+import Calculator from "./Calculator"
+import allReducers from "./reducers"
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Calculator />
     </Provider>,
   document.getElementById('root')
 );
